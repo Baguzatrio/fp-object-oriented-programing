56dd88b53360b1d72afbd877529d876253f9e06f
M. Dwi Nurcahyo <143940804+Nurcahyoo@users.noreply.github.com>
2025-06-01 19:53:04 +0700

    Add files via upload

---
+++ FinalProjectPBO/src/db/Koneksi.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/ikanlautbarokah";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = ""; //
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+            return null;
+        }
+    }
+}
